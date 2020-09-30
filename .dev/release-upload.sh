8024ba5ab15b8e0da26b21e145438003369f6b9b
Sergei S <div@altertech.com>
2020-09-30 05:20:02 +0200

    upload fix

--- .dev/release-upload.sh
+++ .dev/release-upload.sh
@@ -10,11 +10,11 @@ fi
 MIME_TYPE=$(file --mime-type $FILE|awk '{ print $2 }')
 
 GITHUB_TOKEN=$(cat ~/.keys/github)
-ID=$(curl -s https://api.github.com/repos/alttch/pulr/releases | jq -r .\[0\].id)
+ID=$(curl -s https://api.github.com/repos/alttch/modbusgw/releases | jq -r .\[0\].id)
 UPLOAD_ID=$(curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
     -H "Content-Type: $MIME_TYPE" \
     --data-binary @${FILE} \
-    "https://uploads.github.com/repos/alttch/pulr/releases/${ID}/assets?name=$1" | jq -r .id)
+    "https://uploads.github.com/repos/alttch/modbusgw/releases/${ID}/assets?name=$1" | jq -r .id)
 [ -z "$UPLOAD_ID" ] && exit 1
 [ "$UPLOAD_ID" = "null" ] && exit 1
 echo "Uploaded $1 ($FILE : $MIME_TYPE)"
