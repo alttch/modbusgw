340917affe819ffb6ddfa8f2510d07e34bde02c6
Sergei S <div@altertech.com>
2020-10-04 06:26:31 +0200

    1.0.2

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use std::time::Duration;
 use argparse::{ArgumentParser, Store};
 
 const HOMEPAGE: &str = "https://github.com/alttch/modbusgw";
-const VERSION: &str = "1.0.1";
+const VERSION: &str = "1.0.2";
 
 struct Task {
     frame: Vec<u8>,
